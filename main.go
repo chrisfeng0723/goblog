d08df199ae581d8cd6c48c00cc2833bc26fad5c1
fengxinlei <fengxinlei@360.cn>
2019-09-26 16:54:46 +0800

    if and for

--- main.go
+++ main.go
@@ -1,10 +1,39 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
-func main(){
-	http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
-		w.Write([]byte("hello world"))
+type User struct {
+	UserName string
+}
+
+type IndexViewModel struct {
+	Title string
+	User  User
+	Posts []Post
+}
+
+type Post struct {
+	User User
+	Body string
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		u1 := User{UserName: "chris"}
+		u2 :=User{UserName:"rain"}
+
+		posts :=[]Post{
+			Post{User:u1,Body:"i am chris"},
+			Post{User:u2,Body:"I am rain"},
+		}
+		v := IndexViewModel{Title: "Home Page", User: u1,Posts:posts}
+		tpl, _ := template.ParseFiles("view/index.html")
+		fmt.Println(v)
+		tpl.Execute(w, &v)
 	})
-	http.ListenAndServe(":8888",nil)
+	http.ListenAndServe(":8888", nil)
 }
